63479e4ba649e17d5b77211a6ccd57f1244c6698
vinay <vinaykumar.upec@gmail.com>
2026-03-13 22:51:22 +0100

    fix format errors

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-403145438351-us-east-1-an"
-    key            = "eks/terraform.tfstate"
-    region         = "us-east-1"
+    bucket = "terraform-state-403145438351-us-east-1-an"
+    key    = "eks/terraform.tfstate"
+    region = "us-east-1"
   }
 }
