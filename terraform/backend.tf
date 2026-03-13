71378852656e8def257e595fdc1329422d4f8278
vinay <vinay@Mac.lan>
2026-03-13 22:26:47 +0100

    adding backend config

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket         = "terraform-state-403145438351-us-east-1-an"
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
   }
 }
