2d3ac75612bbd8313c780e5a540d77ffb96110e6
vinay <vinaykumar.upec@gmail.com>
2026-03-13 22:47:04 +0100

    fix pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,22 +12,22 @@ module "vpc" {
   enable_nat_gateway = true
 }
 
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "20.0"
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "20.0"
 
-  cluster_name    = var.cluster_name
-  cluster_version = "1.29"
+#   cluster_name    = var.cluster_name
+#   cluster_version = "1.29"
 
-  subnet_ids = module.vpc.private_subnets
-  vpc_id     = module.vpc.vpc_id
+#   subnet_ids = module.vpc.private_subnets
+#   vpc_id     = module.vpc.vpc_id
 
-  eks_managed_node_groups = {
-    default = {
-      instance_types = ["t3.medium"]
-      desired_size   = 2
-      max_size       = 3
-      min_size       = 1
-    }
-  }
-}
+#   eks_managed_node_groups = {
+#     default = {
+#       instance_types = ["t3.medium"]
+#       desired_size   = 2
+#       max_size       = 3
+#       min_size       = 1
+#     }
+#   }
+# }
