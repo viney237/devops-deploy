0703ceaa9368979477bc054f18d68d82b7fb6826
vinay <vinaykumar.upec@gmail.com>
2026-03-13 22:49:51 +0100

    fix state lock error

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,6 +3,5 @@ terraform {
     bucket         = "terraform-state-403145438351-us-east-1-an"
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
   }
 }
