8bc0908d64b539ddc00ecfacf0e059745309d801
vinay <vinaykumar.upec@gmail.com>
2026-03-14 23:05:32 +0100

    fix azs

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name = "eks-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-3a", "eu-west-3b"]
+  azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
 
